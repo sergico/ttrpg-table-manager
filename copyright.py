3ca4162266b1c91d97f9191d4fa5054d5c861d44
Sergio Borghese <s.borghese@netresults.it>
2026-01-02 18:17:03 +0100

    copyright header template

---
+++ copyright.py
@@ -0,0 +1,12 @@
+```
+"""{module.py}: Description of what module.py does."""
+
+__author__      = "Sergio Borghese"
+__copyright__   = "Copyright 2026, "
+__credits__     = []
+__license__     = "GPL"
+__version__		= "3.0"
+__email__       = "sergio.borghese@gmail.com"
+__status__      = "MVP"
+```
+
